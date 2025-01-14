841951d8e55e24e96479418dd94a57355d33bf0f
Kieran Llarena <baymax709@gmail.com>
2025-01-14 15:02:19 -0500

    refactor(frontend): put control panel in /dashboard route + setup route to login page

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import ControlPanel from "@/components/pages/ControlPanel/ControlPanel";
+
+export default function Home() {
+  return <ControlPanel></ControlPanel>;
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,15 @@
-import ControlPanel from "@/components/pages/ControlPanel/ControlPanel";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import spinningCat from "../../public/spinning-cat.gif";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <ControlPanel></ControlPanel>;
+  return (
+    <section className="flex flex-col w-dvh h-dvh items-center justify-center gap-2">
+      <Image src={spinningCat} alt="Spinning cat" />
+      <Button>
+        <LoginLink>Enter Manny Control Panel</LoginLink>
+      </Button>
+    </section>
+  );
 }
